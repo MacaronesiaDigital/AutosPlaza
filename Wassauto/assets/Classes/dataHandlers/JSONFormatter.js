e488d5ebd24576a5201b57188e6e02aed3af9884
OliverAlamoHolm <oliver.alamoholm@gmail.com>
2023-11-08 16:46:31 +0000

    testNumber

--- Wassauto/assets/Classes/dataHandlers/JSONFormatter.js
+++ Wassauto/assets/Classes/dataHandlers/JSONFormatter.js
@@ -98,7 +98,7 @@ async function bookingJSON(unformattedJSON, filePath) {
             const phoneNumberString = element['__EMPTY_14'];
             //let testNumber = phoneNumberString.replace(" ", "");
             testNumber = phoneNumberString.replace("Telf.:", "");
-            const firstNumber = inputString.split(' ');
+            const firstNumber = testNumber.split(' ');
 
             if(firstNumber.length < 6) {
               ii++;
