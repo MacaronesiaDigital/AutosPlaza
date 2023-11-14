6a98b2b7a6b719661d7aa1d7d157a3d692df682c
OliverAlamoHolm <oliver.alamoholm@gmail.com>
2023-11-14 18:19:25 +0000

    .

--- Wassauto/assets/Classes/connections/MongoBDConnection.js
+++ Wassauto/assets/Classes/connections/MongoBDConnection.js
@@ -3,9 +3,9 @@ const { json } = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const ServerApiVersion = require('mongodb').ServerApiVersion;
 // Connection URL
-const dbUrl = 'mongodb://127.0.0.1:27017';
+//const dbUrl = 'mongodb://127.0.0.1:27017';
 //const dbUrl = 'mongodb://https://dff2-206-204-150-110.ngrok-free.app:27017';
-//const dbUrl = 'mongodb://autosplaza:autosplaza1234@autosplaza.macaronesiadigital.com:27017/test';
+const dbUrl = 'mongodb://autosplaza:autosplaza1234@autosplaza.macaronesiadigital.com:27017/test';
 //const dbUrl = 'mongodb+srv://autosplaza:Emiymv9mqFMSl3DQ@cluster0.yszwom4.mongodb.net/?retryWrites=true&w=majority';
 // Database Name
 const dbName = 'AutosPlazaBBDD';
