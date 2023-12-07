2fc1965d03cd8afe67aa35dbcf754a36da47b7ff
OliverAlamoHolm <oliver.alamoholm@gmail.com>
2023-12-07 15:33:51 +0000

    .

--- Wassauto/index.js
+++ Wassauto/index.js
@@ -337,6 +337,7 @@ app.get('/vehiculoform', async (req, res) => {
 
 //Updates the bookings on the database with the data received from /formulario.
 app.post('/updateBooking', upload.any('carImages'), async (req, res) => {
+    console.log(req.body)
     if(testCounter == 1){
         testCounter = 0; 
         return;
